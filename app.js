1812484065d8f77e4e034acc747ba79b39f11769
TegarMachfudzi <77537261+TegarMachfudzi@users.noreply.github.com>
2021-11-13 12:44:24 +0700

    semoga fix

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ const apiRouter = require('./routes/api');
 var cors = require ('cors')
 var app = express();
 
+app.use(cors())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -46,7 +47,7 @@ app.use('/users', usersRouter);
 //admin
 app.use('/admin', adminRouter);
 app.use('/api/v1/member', apiRouter); 
-app.use(cors())
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
